f11b95c8d1a093af4f730f7c1ba705a4ac4d810f
Dian Pan <dian.di.pan@gmail.com>
2015-12-09 22:33:41 -0800

    Update 05_higher_order.js

--- 05_higher_order.js
+++ 05_higher_order.js
@@ -7,20 +7,7 @@ function FirstReverse(str) {
   return str; 
 }
 
-
-//higher order functions allows us to abstract over actions, not just values
-//functions that create new functions 
-
 //Flatten
 var arrays = [[1, 2, 3], [4, 5], [6]];
-var one = arrays[0];
-var two = arrays[1];
-var three = arrays[2];
-var reduce = one.concat(two, three);
-console.log(reduce);
-
-//using reduce 
-var arrays = [[1, 2, 3], [4, 5], [6]];
-console.log(reduce(1,2,3){
-	concat 
-})
+var flatten = arrays[0].concat(arrays[1], arrays[2]);
+console.log(flatten);
